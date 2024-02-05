fe04e3a550f07e5d97b02a3a8260b75a483749dc
abdhafeez17 <abdulhafeez.techy@gmail.com>
2024-02-05 15:10:56 +0530

    e commerce web

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Script for navigation bar
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar")
+const close = document.getElementById("close")
+
+if(bar){// creating a function for clicking the bar
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active')
+    })
+}
+if(close){//on click remove to normal
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active')
+    })
+}
+
